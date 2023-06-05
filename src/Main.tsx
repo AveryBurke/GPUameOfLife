1fd334cff04f6851cd8021e4a36a5eaaf140e75e
Avery Burke <DrFroid01@gmail.com>
2023-06-05 16:36:20 -0700

    Updates

--- src/Main.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./styles.css";
-
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-     <div className="container"><canvas width="512" height="512"></canvas></div>
-  </React.StrictMode>
-);
